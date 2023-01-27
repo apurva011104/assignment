4068d96e40e486379b9afafc8926857782c08115
apurva011104 <122462905+apurva011104@users.noreply.github.com>
2023-01-28 03:11:48 +0530

    Update Sum of elements of different rows and columns.cpp

--- Sum of elements of different rows and columns.cpp
+++ Sum of elements of different rows and columns.cpp
@@ -1,44 +1,39 @@
 #include<stdio.h>
 int main()
 {
-	int m,n,sr=0,sc=0;
-	printf("Enter number of rows:");
+	int m,n;
+	printf("Enter number of rows: ");
 	scanf("%d",&m);
-	printf("Enter number f columns:");
+	printf("Enter number of columns: ");
 	scanf("%d",&n);
 	int a[m][n];
+	int sc=0,sr=0;
 	for(int i=0;i<m;i++)
 	{
 		for(int j=0;j<n;j++)
 		{
+			printf("Enter value of element: ");
 			scanf("%d",&a[i][j]);
 		}
 	}
 	for(int i=0;i<m;i++)
 	{
+		sr=0;
 		for(int j=0;j<n;j++)
 		{
 			printf("%d\t",a[i][j]);
-		}
-		printf("\n");
-	}
-	for(int i=0;i<m;i++)
-	{
-		for(int j=0;j<n;j++)
-		{
 			sr=sr+a[i][j];
 		}
-		printf("\nSum of elements of row %d= %d",i,sr);
-		sr=0;
+		printf("%d\n",sr);
 	}
-	for(int j=0;j<n;j++)
+	for(int i=0;i<n;i++)
 	{
-		for(int i=0;i<m;i++)
-		{
-			sc=sc+a[i][j];
-		}
-		printf("\nSum of elements of column %d= %d",j,sc);
 		sc=0;
+		for(int j=0;j<m;j++)
+		{
+			sc=sc+a[j][i];
+		}
+		printf("%d\t",sc);
 	}
 	return 0;
 }
